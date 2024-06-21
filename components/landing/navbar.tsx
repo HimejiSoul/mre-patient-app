a1205962933e89e5e66f2d41add7e0ffbae30e0a
MTharreq <mtharreq@gmail.com>
2024-06-21 23:39:34 +0700

    feat: update navlink components

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -35,8 +35,8 @@ export function Navbar() {
       id="header"
       className="sticky top-0 z-50 w-full bg-white py-2 drop-shadow"
     >
-      <div className="container mx-auto mt-0 flex w-full flex-wrap items-center justify-between px-6 py-1 md:py-3">
-        <Link href="/">
+      <div className="container mx-auto mt-0 flex items-center justify-between px-6 py-1 md:py-3">
+        <Link href="/" className="md:w-1/4">
           <Image
             priority
             src={Logo}
@@ -47,30 +47,47 @@ export function Navbar() {
         </Link>
         <Sheet>
           <SheetTrigger asChild>
-            <Button variant="ghost" size={'icon'} className="md:hidden">
-              <Menu color="#2563eb" />
+            <Button
+              variant="ghost"
+              size={'icon'}
+              className="justify-self-end md:hidden"
+            >
+              <Menu color="#60b7eb" />
             </Button>
           </SheetTrigger>
           <SheetContent
             side={'right'}
-            className="flex w-3/4 flex-col gap-4 bg-white py-8 pt-16"
+            className="z-[80] flex w-3/4 flex-col gap-4 bg-white py-8 pt-16"
           >
             {links.map((link, i) => (
               <SheetClose key={i} asChild>
                 <Link
                   href={link.href}
-                  className="rounded-lg bg-white py-2 text-center font-semibold text-rme-blue-500 hover:bg-rme-blue-500/10"
+                  className={`${
+                    link.href === path
+                      ? 'bg-sky-50 text-rme-blue-500'
+                      : 'text-slate-500'
+                  } rounded-lg px-4 py-2 font-semibold hover:bg-[#60b7eb]/10`}
                 >
                   {link.name}
                 </Link>
               </SheetClose>
             ))}
+            <SheetClose asChild>
+              <Link
+                href="/reservasi-layanan"
+                className={`${
+                  '/reservasi-layanan' === path
+                    ? 'bg-sky-50 text-rme-blue-500'
+                    : 'text-slate-500'
+                } rounded-lg px-4 py-2 font-semibold hover:bg-[#60b7eb]/10`}
+              >
+                Reservasi Layanan
+              </Link>
+            </SheetClose>
           </SheetContent>
         </Sheet>
-        <div
-          className="order-3 hidden w-full md:order-1 md:flex md:w-auto md:items-center"
-          id="menu"
-        >
+        <div className="hidden justify-self-center md:flex" id="menu">
           <nav>
             <ul className="flex gap-2">
               {links.map((link, i) => (
@@ -78,29 +95,29 @@ export function Navbar() {
                   <Link
                     className={`${
                       link.href === path
-                        ? 'bg-sky-50 text-blue-500'
-                        : 'text-[#60b7eb]'
-                    } rounded-lg px-4 py-2 text-center font-semibold transition duration-300 ease-out hover:bg-rme-blue-500/10`}
+                        ? 'bg-sky-50 font-semibold text-rme-blue-500 hover:bg-rme-blue-500/10'
+                        : 'text-slate-500 hover:bg-slate-500/10'
+                    } rounded-lg px-4 py-2 text-center font-normal transition duration-300 ease-out`}
                     href={link.href}
                   >
                     {link.name}
                   </Link>
                 </li>
               ))}
-              {/* <li>
-                <Link
-                  className={`${
-                    '/reservasi-layanan' === path
-                      ? 'bg-sky-50 text-blue-500'
-                      : 'text-[#60b7eb]'
-                  } rounded-full border border-blue-500 px-4 py-2 text-center font-semibold transition duration-300 ease-out hover:bg-rme-blue-500/10`}
-                  href={'/reservasi-layanan'}
-                >
-                  Lakukan Reservasi
-                </Link>
-              </li> */}
             </ul>
           </nav>
+        </div>
+        <div className="hidden w-1/4 justify-end md:flex">
+          <Link
+            className={`${
+              '/reservasi-layanan' === path
+                ? 'bg-[#60b7eb] text-white hover:bg-[#60b7eb]/90'
+                : 'text-[#60b7eb] hover:bg-rme-blue-500/10'
+            } w-fit rounded-full border border-[#60b7eb] px-4 py-2 text-center font-semibold transition duration-300 ease-out`}
+            href={'/reservasi-layanan'}
+          >
+            Lakukan Reservasi
+          </Link>
         </div>
       </div>
     </nav>
